28e4aec2446e88d69618251aa73b4b0f704f1288
Alan Zambrano <alnzam17@gmail.com>
2023-01-21 13:39:07 -0600

    fix: :wrench: change base path for deployments

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://zamancer.github.io",
-    base: "/scorbunny-blog"
+    site: "https://zamancer.github.io"
 });
